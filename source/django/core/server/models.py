ed0bcda08135a58ee13d8c766b9876b4cbe8d26f
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-05 23:08:08 -0400

    feat(models): create User model with fields for username, email, firstname, lastname, and password hash

---
+++ source/django/core/server/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    passwordhash = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
